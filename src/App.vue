aff569f37e19aa4da499e72f3eed0292c9068ccf
Tonderai Muchada <tonderaimuchada@gmail.com>
2026-04-21 21:03:08 +0200

    init: Add skeletal structure for leads properties

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+    <div>
+        <div class="card">
+            <div class="card-header">
+                <h4>
+                    Leads
+                    <RouteLink to="/lead/create" class="btn btn-primary float-end">
+                        Add Lead
+                    </RouteLink>
+                </h4>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Company</th>
+                            <th>Job Title</th>
+                            <th>Email</th>
+                            <th>Phone Number</th>
+                            <th>Address</th>
+                            <th>Date Created</th>
+                        </tr>
+                    </thead>
+                    <tbody v-if="this.leads.length > 0">
+                        <tr v-for="(lead, index) in this.leads" :key="index">
+                            <td>{{ lead.id }}</td>
+                            <td>{{ lead.firstName }}</td>
+                            <td>{{ lead.lastName }}</td>
+                            <td>{{ lead.companyName }}</td>
+                            <td>{{ lead.jobTitle }}</td>
+                            <td>{{ lead.email }}</td>
+                            <td>{{ lead.phoneNumber }}</td>
+                            <td>{{ lead.address }}</td>
+                            <td>{{ lead.dateCreated }}</td>                            
+                            <td>
+                                <RouterLink to="/" class="btn btn-success float-end">Edit</RouterLink>
+                                <button type="button" class="btn btn-danger float-end">Delete</button>
+                            </td>                            
+                        </tr>
+                    </tbody>
+                    <tbody v-else>
+                        <tr>
+                            <td colspan="9">Loading...</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'leads',
+    data() {
+        return {
+            leads: []
+        }
+    },
+    mounted(){
+        this.getLeads();
+        
+    },
+    methods: {
+        getLeads() {
+            //For Spring Boot: axios.get('http://localhost:8080/api/lead').then(res => {
+            axios.get('http://localhost:8000/api/lead').then(res => {
+                leads = res.data.leads;
+            });
+        }
+    }
+}
+</script>
