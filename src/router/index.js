2452400bad25df648ecde2c2dddea7da33fb4ce7
Tonderai Muchada <tonderaimuchada@gmail.com>
2026-04-23 08:32:53 +0200

    feature: Add lead creation logic

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import AppVue from ''
-import LeadCreate from '../views/LeadCreate.vue'
+import LeadCreate from '/views'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
-            path: 'leads',
+            path: '',
             name: 'home',
             component: AppVue
         },
         {
-            path: 'leads/create',
+            path: '/leads/create',
             name: 'leadCreate',
             component: LeadCreate
         }
     ]
-});
+})
 
 export default router
--- src/views/LeadCreate.vue
+++ src/views/LeadCreate.vue
@@ -6,33 +6,70 @@
             </div>
             <div class="card-body">
                 <div class="mb-3">
-                    <label for="">Name</label>
-                    <input type="text" class="form-control"></input>
+                    <label for="">Full Name</label>
+                    <input type="text" v-model="model.lead.fullName" class="form-control"></input>
                 </div>
                 <div class="mb-3">
-                    <label for="">Name</label>
-                    <input type="text" class="form-control"></input>
+                    <label for="">Email Address</label>
+                    <input type="text" v-model="model.lead.emailAddress" class="form-control"></input>
                 </div>
                 <div class="mb-3">
-                    <label for="">Name</label>
-                    <input type="text" class="form-control"></input>
+                    <label for="">Phone Number</label>
+                    <input type="text" v-model="model.lead.phoneNumber" class="form-control"></input>
                 </div>
                 <div class="mb-3">
-                    <label for="">Name</label>
-                    <input type="text" class="form-control"></input>
+                    <label for="">Company Name</label>
+                    <input type="text" v-model="model.lead.companyName" class="form-control"></input>
                 </div>
                 <div class="mb-3">
-                    <label for="">Name</label>
-                    <input type="text" class="form-control"></input>
+                    <label for="">Status</label>
+                    <input type="text" v-model="model.lead.status" class="form-control"></input>
                 </div>
                 <div class="mb-3">
-                    <label for="">Name</label>
-                    <input type="text" class="form-control"></input>
-                </div>
-                <div class="mb-3">
-                    <button type="button" class="btn btn-primary">Save</button>
+                    <button type="button" @click="saveLead" class="btn btn-primary">Save</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'leadCreate',
+    data(){
+        return {
+            model: {
+                lead: {
+                    fullName: '',
+                    emailAddress: '',
+                    phoneNumber: '',
+                    companyName: '',
+                    status: ''
+                }
+            }
+        }
+    },
+    methods: {
+        saveLead(){
+            axios.post('https://loc', this.model.lead)
+            .then(res => {
+                console.log(res.data);
+                this.model.lead = {
+                    fullName: '',
+                    emailAddress: '',
+                    phoneNumber: '',
+                    companyName: '',
+                    status: ''
+                }               
+            })
+            .catch(function () {
+                if (error.response) {
+                    
+                }
+            })
+        }
+    }
+}
+</script>
